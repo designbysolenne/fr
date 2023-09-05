ead5c06f6f7e8fafadc3afd44ca4d6d6d906e12d
mlejeune <mlejeunepro@gmail.com>
2023-09-05 19:07:12 +0200

    feat: language

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={inter.className}>
       <Header/>
       {children}
