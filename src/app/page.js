01d3d68510e4a2cb384336d8f2cbef5b40ee692f
mlejeune <mlejeunepro@gmail.com>
2023-09-05 10:05:25 +0200

    gitignore

--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,7 @@ export default function Home() {
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Find  poueoruin-depth information about Next.js features and API.</p>
         </a>
 
         <a
