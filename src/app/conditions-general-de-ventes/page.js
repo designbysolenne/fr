e35af65cbfc41cc83b035f3c3e8972cdb8f14a8a
mlejeune <mlejeunepro@gmail.com>
2023-09-05 12:04:54 +0200

    feat: pages

---
+++ src/app/conditions-general-de-ventes/page.js
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+
+export default function cgv() {
+    return (
+        <main className={styles.main}>
+            <h1>CGV</h1>
+        </main>
+    )
+}
---
+++ src/app/mentions-legales/page.js
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+
+export default function legacy() {
+    return (
+        <main className={styles.main}>
+            <h1>Mentions-Légales</h1>
+        </main>
+    )
+}
