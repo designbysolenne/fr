66df57df358f383c5823ade3abf52d8c4961a69f
mlejeune <mlejeunepro@gmail.com>
2023-09-13 18:09:20 +0200

    feat: remove contact bloc

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Contact from "@/app/components/Contact";
 import Jumbotron from "@/app/components/Jumbotron";
 import Services from "@/app/components/Services";
 import Portfolio from "@/app/components/Portfolio";
@@ -13,7 +12,6 @@ export default function Home() {
         <HowWork/>
         <Portfolio/>
         <Testimonials/>
-        <Contact/>
     </div>
   )
 }
