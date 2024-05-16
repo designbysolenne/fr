33294f369399a005b8f78fbcc2444777a1c7df58
mlejeune <mlejeunepro@gmail.com>
2024-05-16 20:38:05 +0200

    feat: :fire: update 16/05/24 filter portfolio

--- src/app/components/Portfolio.js
+++ src/app/components/Portfolio.js
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from 'react'
+
 import Image from 'next/image';
 
 import Shape from '@/app/assets/img/cover/shape-cross.svg';
@@ -13,6 +17,21 @@ import MoutainTrailFeedInstagram from "../assets/img/moutaintrail-feedinsta.jpg"
 import SpaarpleCarte from "../assets/img/spaarple-carte.jpg";
 
 export default function Portfolio() {
+    const [filter, setFilter] = useState('all');
+
+    const projects = [
+        { id: 1, name: 'Identité visuelle Pia', type: 'identite-visuelle', image: LogoPia, modalId: 'logoPiaModal' },
+        { id: 2, name: 'Packaging compléments alimentaires', type: 'packaging', image: PiaPackaging, modalId: 'PiaPackaging' },
+        { id: 9, name: 'Carte de visite pour agence Web', type: 'support-communication', image: SpaarpleCarte, modalId: 'SpaarpleCarte' },
+        { id: 3, name: 'Carte de visite restaurant Sicilien', type: 'support-communication', image: SanMarcoCarte, modalId: 'SanMarcoCarte' },
+        { id: 4, name: 'Identité visuelle GardeOToit', type: 'identite-visuelle', image: GardeOToitLogo, modalId: 'GardeOToitLogo' },
+        { id: 5, name: 'Boite de conserve pâté animal', type: 'packaging', image: GardeOToitPackaging, modalId: 'GardeOToitPackaging' },
+        { id: 6, name: 'Flyer de présentation', type: 'support-communication', image: GardeOToitAffiche, modalId: 'GardeOToitAffiche' },
+        { id: 7, name: 'Identité visuelle MontainTrail', type: 'identite-visuelle', image: MoutainTrailLogo, modalId: 'MoutainTrailLogo' },
+        { id: 8, name: 'Feed instagram marque de randonnée', type: 'support-communication', image: MoutainTrailFeedInstagram, modalId: 'MoutainTrailFeedInstagram' },
+    ];
+
+    const filteredProjects = projects.filter(project => filter === 'all' || project.type === filter);
 
     return (
         <div id="realisation" className="container">
@@ -26,233 +45,35 @@ export default function Portfolio() {
             <div className="container text-center">
                 <div className="row gx-5">
                     <div className="col-12 mb-5">
-                        <button id="identite-visuelle" className="mx-1 p-2 border rounded-1 btn-primary">Tous les
-                            projets
+                        <button onClick={() => setFilter('all')}
+                                className="mx-1 p-2 border rounded-1 btn-primary">Tous les projets
                         </button>
-                        <button id="identite-visuelle" className="mx-1 p-2 border rounded-1 btn-primary">Identité
-                            visuelle
+                        <button onClick={() => setFilter('identite-visuelle')}
+                                className="mx-1 p-2 border rounded-1 btn-primary">Identité visuelle
                         </button>
-                        <button className="mx-1 p-2 border rounded-1 btn-primary">Packaging</button>
-                        <button className="mx-1 p-2 border rounded-1 btn-primary">Templates Instagram</button>
+                        <button onClick={() => setFilter('packaging')}
+                                className="mx-1 p-2 border rounded-1 btn-primary">Packaging
+                        </button>
+                        <button onClick={() => setFilter('support-communication')}
+                                className="mx-1 p-2 border rounded-1 btn-primary">Supports de communication
+                        </button>
                     </div>
                 </div>
             </div>
 
-            <div className="d-flex justify-content-center">
+            <div className="container">
                 <div className="row">
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={LogoPia} data-bs-target="#logoPiaModal" data-bs-toggle="modal">
-                                <Image className="identite-visuelle" src={LogoPia} alt="Réseaux Sociaux"
-                                       height={300} width={300}
-                                />
-                            </a>
+                    {filteredProjects.map((project, index) => (
+                        <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center" key={project.id}>
+                            <div className="card rounded-1">
+                                    <Image className={project.type} src={project.image} alt={project.name}
+                                           height={300} width={300}
+                                    />
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={SanMarcoCarte} data-bs-target="#SanMarcoCarte" data-bs-toggle="modal">
-                                <Image src={SanMarcoCarte} className="" alt="Réseaux Sociaux"
-                                       height={300} width={300}/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={GardeOToitLogo} data-bs-target="#GardeOToitLogo" data-bs-toggle="modal">
-                                <Image src={GardeOToitLogo} className="" alt="Réseaux Sociaux"
-                                       height={300} width={300}/>
-                            </a>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
-            <div className="d-flex justify-content-center">
-                <div className="row">
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={GardeOToitAffiche} data-bs-target="#GardeOToitAffiche" data-bs-toggle="modal">
-                                <Image className="identite-visuelle" src={GardeOToitAffiche} alt="Réseaux Sociaux"
-                                       height={300} width={300}
-                                />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={PiaPackaging} data-bs-target="#PiaPackaging" data-bs-toggle="modal">
-                                <Image src={PiaPackaging} className="" alt="Réseaux Sociaux"
-                                       height={300} width={300}/>
-                                </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={MoutainTrailFeedInstagram} data-bs-target="#MoutainTrailFeedInstagram" data-bs-toggle="modal">
-                                <Image src={MoutainTrailFeedInstagram} className="" alt="Réseaux Sociaux"
-                                       height={300} width={300}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="d-flex justify-content-center">
-                <div className="row">
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={SpaarpleCarte} data-bs-target="#SpaarpleCarte" data-bs-toggle="modal">
-                                <Image className="identite-visuelle" src={SpaarpleCarte} alt="Réseaux Sociaux"
-                                       height={300} width={300}
-                                />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={GardeOToitPackaging} data-bs-target="#GardeOToitPackaging" data-bs-toggle="modal">
-                                <Image src={GardeOToitPackaging} className="" alt="Réseaux Sociaux"
-                                       height={300} width={300}/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6 mb-6 d-flex justify-content-center">
-                        <div className="card rounded-1">
-                            <a href={MoutainTrailLogo} data-bs-target="#MoutainTrailLogo" data-bs-toggle="modal">
-                                <Image src={MoutainTrailLogo} className="" alt="Réseaux Sociaux"
-                                       height={300} width={300}/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* Fist Row */}
-            <div className="modal fade rounded-top show" id="logoPiaModal" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={LogoPia} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal fade rounded-top show" id="SanMarcoCarte" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={SanMarcoCarte} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal fade rounded-top show" id="GardeOToitLogo" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={GardeOToitLogo} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* End First Row */}
-
-            {/* Second Row */}
-            <div className="modal fade rounded-top show" id="GardeOToitAffiche" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={GardeOToitAffiche} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal fade rounded-top show" id="PiaPackaging" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={PiaPackaging} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal fade rounded-top show" id="MoutainTrailFeedInstagram" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={MoutainTrailFeedInstagram} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* End Second Row */}
-
-            {/* Three Row */}
-            <div className="modal fade rounded-top show" id="SpaarpleCarte" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={SpaarpleCarte} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal fade rounded-top show" id="GardeOToitPackaging" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={GardeOToitPackaging} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal fade rounded-top show" id="MoutainTrailLogo" aria-hidden="true"
-                 aria-labelledby="" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content bg-transparent border-0 text-center">
-                        <div className="modal-body container-fluid overflow-auto p-0">
-                            <Image className="identite-visuelle img-fluid"
-                                   src={MoutainTrailLogo} alt="Réseaux Sociaux"
-                                   height={800} width={800}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* End Three Row */}
-
-
         </div>
     )
 }
